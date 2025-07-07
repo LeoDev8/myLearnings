7552ea15c86fba767bf964d3f529918f0d18e6f7
Leo <zhihao_2023@163.com>
2025-07-07 21:47:26 +0800

    CS61A(lab03.py) Finish Question2 in lab03

---
+++ 01-CS61A<Structure and Interpretation of Computer Programs>/lab03/lab03.py
@@ -0,0 +1,84 @@
+SOURCE_FILE = __file__
+
+
+def double_eights(n):
+    """Returns whether or not n has two digits in row that
+    are the number 8.
+
+    >>> double_eights(1288)
+    True
+    >>> double_eights(880)
+    True
+    >>> double_eights(538835)
+    True
+    >>> double_eights(284682)
+    False
+    >>> double_eights(588138)
+    True
+    >>> double_eights(78)
+    False
+    >>> # ban iteration
+    >>> from construct_check import check
+    >>> check(SOURCE_FILE, 'double_eights', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n < 10:
+        return False
+    else:
+        return double_eights(n // 10) or (n % 100) % 88 == 0
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    "*** YOUR CODE HERE ***"
+    if a == 1 or b == 1:
+        return 1
+    if a % b == 0:
+        return b
+    if b % a == 0:
+        return a
+    else:
+        return gcd(b, a % b) if a > b else gcd(a, b % a)
+
+
+def pascal(row, column):
+    """Returns the value of the item in Pascal's Triangle
+    whose position is specified by row and column.
+    >>> pascal(0, 0)    # The top left (the point of the triangle)
+    1
+    >>> pascal(0, 5)	# Empty entry; outside of Pascal's Triangle
+    0
+    >>> pascal(3, 2)	# Row 3 (1 3 3 1), Column 2
+    3
+    >>> pascal(4, 2)     # Row 4 (1 4 6 4 1), Column 2
+    6
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def paths(m, n):
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
