292f826124838bc966bc0fbf1c8e88c7d53c5259
Leo <zhihao_2023@163.com>
2025-08-05 20:29:18 +0800

    CS61A(lab10) Initialize Lab 10 Folder

---
+++ 01-CS61A<Structure and Interpretation of Computer Programs>/lab10/lab10.scm
@@ -0,0 +1,11 @@
+(define (exp b n)
+  (define (helper n so-far) "YOUR CODE HERE")
+  (helper n 1))
+
+(define (last s) 'YOUR-CODE-HERE)
+
+(define (swap s) 'YOUR-CODE-HERE)
+
+(define (concatenate s) 'YOUR-CODE-HERE)
+
+(define (filter pred lst) 'YOUR-CODE-HERE)
